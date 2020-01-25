a8402cf4378ba3a6277c4f41a0a9b5424738559c
Edwin Elia <edwinelia@google.com>
2020-01-25 15:31:11 -0800

    Initialize Replicator project

--- cdap-ui/app/cdap/components/Home/index.js
+++ cdap-ui/app/cdap/components/Home/index.js
@@ -105,6 +105,11 @@ const ExperimentToggle = Loadable({
   loading: LoadingSVGCentered,
 });
 
+const Replicator = Loadable({
+  loader: () => import(/* webpackChunkMame: "Replicator" */ 'components/Replicator'),
+  loading: LoadingSVGCentered,
+});
+
 export default class Home extends Component {
   componentWillMount() {
     NamespaceStore.dispatch({
@@ -156,6 +161,7 @@ export default class Home extends Component {
           <Route path="/ns/:namespace/securekeys" component={SecureKeys} />
           <Route path="/ns/:namespace/kitchen" component={ConfigurationGroupKitchenSync} />
           <Route path="/ns/:namespace/experimentToggle" component={ExperimentToggle} />
+          <Route path="/ns/:namespace/replicator" component={Replicator} />
           <Route component={Page404} />
         </Switch>
       </div>
---
+++ cdap-ui/app/cdap/components/Replicator/List/SourceList/index.tsx
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import * as React from 'react';
+import { MyReplicatorApi } from 'api/replicator';
+import { getCurrentNamespace } from 'services/NamespaceStore';
+import { PluginType } from 'components/Replicator/constants';
+
+const SourceList: React.FC = () => {
+  const [sources, setSources] = React.useState([]);
+
+  React.useEffect(() => {
+    const params = {
+      namespace: getCurrentNamespace(),
+      pluginType: PluginType.source,
+    };
+
+    MyReplicatorApi.getPlugins(params).subscribe((res) => {
+      // TODO: aggregate versions
+      setSources(res);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Sources</h2>
+
+      <ul>
+        {sources.map((source) => {
+          return <li key={source.name}>{source.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default SourceList;
